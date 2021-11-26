53fdf68c1a7e03178fd19730442f658b810a58d8
gonior <arthemuss14@gmail.com>
2021-11-27 02:43:31 +0700

    commented dotenv module

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 const crypto = require('crypto')
