bfabbb15900f74f198e47cd612edffdb0b8c49c1
Dedi Cahya <dedicahya@pop-os.localdomain>
2022-02-07 19:45:02 +0700

    wow

--- routes/Activity.js
+++ routes/Activity.js
@@ -4,7 +4,7 @@ const Activity = require('../models/Activity')
 router.get('/:id', async (req, res) => {
     
     try {
-        let items = await Activity.find({itemId : req.params.id}).populate('userId', 'name', 'itemId').sort({date : 'desc'})
+        let items = await Activity.find({itemId : req.params.id}).populate('userId', 'name').populate('itemId').sort({date : 'desc'})
         if (items.length > 0) {
             
             res.status(200).json(items)
@@ -16,15 +16,19 @@ router.get('/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
+    
     let limit = req.query.limit ? +req.query.limit : ''
+    console.log(limit)
     try {
-        let items = await Activity.find({}).populate('userId', 'name', 'itemId').sort({date : 'desc'}).limit(limit)
+        let items = await Activity.find({}).populate('userId', 'name').populate('itemId').sort({date : 'desc'}).limit(limit)
+        
         if (items.length > 0) {
             
             res.status(200).json(items)
         }
         else res.status(401).json({message : "Belum ada aktivitas"})
     } catch (error) {
+        console.log(error)
         return res.status(500).json(error)
     }  
 })
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,4 +1,4 @@
-// require('dotenv').config()
+require('dotenv').config()
 const router = require('express').Router()
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
